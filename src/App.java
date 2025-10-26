19487848ce52333c9225e9dd2d28bebfd00c8199
MiguelTR <migueltr.2019@outlook.com>
2025-10-26 18:45:02 +0100

    first commit

---
+++ src/App.java
@@ -0,0 +1,30 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        try {
+            boolean distintos = true;
+            int num1;
+            int num2;
+            do {
+                num1 = Integer.parseInt(System.console().readLine("Introduzca un número entero: "));
+                num2 = Integer
+                        .parseInt(System.console().readLine("Introduzca otro número entero distinto al anterior: "));
+                if (num1 == num2) {
+                    distintos = true;
+                }
+            } while (distintos == false);
+
+            if (num2 < num1) {
+                int aux = num2;
+                num2 = num1;
+                num1 = aux;
+            }
+            for (int i = num1; i <= num2; i += 7) {
+                System.out.print(i + " ");
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Número inválido");
+        } catch (Exception e) {
+            System.out.println("Ha ocurrido un error inesperado");
+        }
+    }
+}
